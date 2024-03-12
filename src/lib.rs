54b1b4672a15ae1fbdac0f7236c0ea3818aac294
Chris <8133496+chriswmann@users.noreply.github.com>
2024-03-12 14:40:43 +0000

    Add initial lib.rs
    This is an incomplete and very simple beginning for a BPE algorithm.
    It just uses characters at the moment and is still missing important
    things like merging the most frequent character pairs.
    It doesn't do anything clever like working asynchronously.
    It doesn't do anything slightly clever like adding word boundary tokens.
    But it's a start.

---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+#![deny(missing_docs)]
+//! Library to provide byte pair encoding of strings.
+
+use std::collections::HashMap;
+use std::fmt::Debug;
+
+/// Define a hashable type to use as a key in a HashMap when counting
+#[derive(Hash, PartialEq, Eq)]
+pub struct TupleStruct(char, char);
+
+impl Debug for TupleStruct {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}, {:?}", self.0, self.1)
+    }
+}
+
+/// Count the number of occurrences of each integer in an iterator
+pub struct Counter {
+    counts: HashMap<TupleStruct, usize>,
+}
+
+/// Create a new counter using Counter()
+impl Default for Counter {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Counter {
+    /// Create a new counter
+    pub fn new() -> Self {
+        Counter {
+            counts: std::collections::HashMap::new(),
+        }
+    }
+
+    /// Count the number of occurrences of each integer in an iterator
+    pub fn count(&mut self, string: &str)
+    {
+        let s_iter = string.chars().zip(string.chars().skip(1));
+        for (c1, c2) in s_iter {
+            let key = TupleStruct(c1, c2);
+            *self.counts.entry(key).or_insert(0) += 1;
+        }
+    }
+
+    /// Get the counts as a HashMap
+    pub fn counts(&self) -> &HashMap<TupleStruct, usize> {
+        &self.counts
+    }
+
+    /// Get the most common pair of characters
+    pub fn most_common(&self) -> Option<&TupleStruct> {
+        self.counts.iter().max_by(|a, b| a.1.cmp(&b.1))
+        .map(|(k, _v)| k)
+
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut counter = Counter::new();
+        let my_string = String::from("Cat at 😃!");
+        counter.count(&my_string);
+        println!("{:?}", counter.counts());
+        assert_eq!(counter.counts().get(&TupleStruct(' ', 'a')), Some(&1));
+        assert_eq!(counter.counts().get(&TupleStruct(' ', '😃')), Some(&1));
+        assert_eq!(counter.counts().get(&TupleStruct('C', 'a')), Some(&1));
+        assert_eq!(counter.counts().get(&TupleStruct('a', 't')), Some(&2));
+        assert_eq!(counter.counts().get(&TupleStruct('t', ' ')), Some(&2));
+        assert_eq!(counter.counts().get(&TupleStruct('😃', '!')), Some(&1));
+    }
+
+}
